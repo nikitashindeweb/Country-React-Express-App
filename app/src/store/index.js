bb7b18da384c244de5c766e4f9e6f7c2968394cf
Nikita <shindenikita111998@gmail.com>
2022-04-23 12:23:55 +0530

    country init

--- app/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { countryReducer } from './countryReducer'
-
-export default configureStore({
-  reducer: {
-    countries: countryReducer,
-  },
-})
